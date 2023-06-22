38f5f1f4535b835144a5c2de7d657a1bf93aeac3
jaredsbt <jaredsbt@gmail.com>
2023-06-22 15:16:20 -0700

    Loaded StyleSheet

---
+++ products/products.js
@@ -0,0 +1,56 @@
+const products = [
+    {
+        id: 0,
+        name: "Shirt",
+        price: 6.00,
+        description: 
+            "",
+        imageSrc: "./nakSnaks/esquites.jpeg",
+        inStock: 5,
+    },
+
+    {
+        id: 1,
+        name: "Shoes",
+        price: 5.00,
+        description: "",
+        imageSrc: "./nakSnaks/rueditas.jpeg",
+        inStock: 5,
+    },
+
+    {
+        id: 2,
+        name: "Hats",
+        price: 3.00,
+        description: "",
+        imageSrc: "./nakSnaks/nachos.jpeg",
+        inStock: 5,
+    },
+
+    {
+        id: 3,
+        name: "Pants",
+        price: 5.00,
+        description: "",
+        imageSrc: "./nakSnaks/hotCheetoswCheese.webp",
+        inStock: 5,
+    }, 
+
+    {
+        id: 4,
+        name: "Hoodies",
+        price: 5.00,
+        description: "",
+        imageSrc: "./nakSnaks/horchata.jpeg",
+        inStock: 5,
+    },
+
+    {
+        id: 5,
+        name: "Beanies",
+        price: 5.00,
+        description: "",
+        imageSrc: "./nakSnaks/chiadrink.jpeg",
+        inStock: 5,
+    },
+]
